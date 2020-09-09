9a20cc0dd0154501e0f18bf5cbde68e64b3dbafc
renanrcp <renancamerapinheiro@gmail.com>
2020-09-09 10:23:55 -0300

    QuartzExceptionJobListener

--- Listeners/IJobExceptionListener.cs
+++ Listeners/IJobExceptionListener.cs
@@ -1,12 +1,11 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace QuartzNETDependencyInversionSample.Listeners
 {
     public interface IJobExceptionListener
     {
-        string Name { get; }
-
-        Task HandleExceptionAsync(Exception jobException);
+        Task HandleExceptionAsync(Exception jobException, CancellationToken cancellationToken = default);
     }
 }
---
+++ Listeners/QuartzExceptionJobListener.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace QuartzNETDependencyInversionSample.Listeners
+{
+    public class QuartzExceptionJobListener : IJobListener
+    {
+        private readonly IEnumerable<IJobExceptionListener> _expceptionListeners;
+
+        private const string NAME = "Quartz Exception Job Listener";
+
+        public QuartzExceptionJobListener(IEnumerable<IJobExceptionListener> expceptionListeners)
+        {
+            _expceptionListeners = expceptionListeners;
+        }
+
+        public string Name => NAME;
+
+        public Task JobExecutionVetoed(IJobExecutionContext context, CancellationToken cancellationToken = default)
+            => Task.CompletedTask;
+        public Task JobToBeExecuted(IJobExecutionContext context, CancellationToken cancellationToken = default)
+            => Task.CompletedTask;
+
+        public Task JobWasExecuted(IJobExecutionContext context, JobExecutionException jobException, CancellationToken cancellationToken = default)
+        {
+            var exception = jobException?.InnerException ?? jobException;
+
+            if (exception == null)
+                return Task.CompletedTask;
+
+            var tasks = _expceptionListeners.Select(a => a.HandleExceptionAsync(exception, cancellationToken));
+
+            return Task.WhenAll(tasks);
+        }
+    }
+}
